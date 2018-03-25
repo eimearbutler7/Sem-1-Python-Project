f015d788cb5fb2c76095e8176c0fd14919bb1ba6
eimearbutler7 <35781826+eimearbutler7@users.noreply.github.com>
2018-03-25 16:08:17 +0100

    Update Fisher_Iris_Script.py

--- Fisher_Iris_Script.py
+++ Fisher_Iris_Script.py
@@ -1,17 +1,35 @@
-#Scripting Notes
-#From: Python Standard Library 9.7. statistics — Mathematical statistics functions
+import statistics
+import csv
 
-#mean() 	Arithmetic mean (“average”) of data.
-#harmonic_mean() 	Harmonic mean of data.
-#median() 	Median (middle value) of data.
-#median_low() 	Low median of data.
-#median_high() 	High median of data.
-#median_grouped() 	Median, or 50th percentile, of grouped data.
-#mode() 	Mode (most common value) of discrete data.
-#max_value = max(somelist)
-#min_value = min(somelist)
-#avg_value = sum(somelist)/len(somelist)
-#pstdev() 	Population standard deviation of data.
-#pvariance() 	Population variance of data.
-#stdev() 	Sample standard deviation of data.
-#variance() 	Sample variance of data.
+z = "Petal Length" #row description, change as needed
+r = 0 #row index number, chenge in accordance with description
+y = [] #this will become the list of entries in the row selected above 
+with open('iris.csv') as f: # open iris file
+  for line in f:
+    iris = line.split(',')[r] # split out each linein iris file
+    y.append(iris) # put each entry from eah row back into the y list as a string 
+
+x = [float(i) for i in y] #change the 'y' string list, into a floating number version list called 'x' https://stackoverflow.com/questions/1614236/in-python-how-do-i-convert-all-of-the-items-in-a-list-to-floats
+
+print(x) #optional to see final list 'x' containing floating number versions of the row from the csv file 
+
+a = statistics.mean(x) # Python Standard Library 9.7. statistics — Mathematical statistics functions
+a = "%.3f" % a     # round to the nthird decimal point  https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
+b = statistics.median(x)
+b = "%.1f" % b
+c = max(x)
+c = "%.1f" % c
+d = min(x)
+d = "%.1f" % d
+e = statistics.stdev(x)
+e = "%.3f" % e
+f = statistics.variance(x)
+f = "%.3f" % f
+
+print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,))
+print("The mean of %s is: %s" %(z, a))
+print("The median of %s is: %s" %(z, b))
+print("The maximum of %s is: %s" %(z, c))
+print("The minimum of %s is: %s" %(z, d))
+print("The standard deviation of %s is: %s" %(z, e))
+print("The standard variance of %s is: %s" %(z, f))
