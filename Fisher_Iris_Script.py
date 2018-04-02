6be1751de1d78d7083eb5dda0b43ec14021969e3
eimearbutler7 <35781826+eimearbutler7@users.noreply.github.com>
2018-04-02 09:19:16 +0100

    Update Fisher_Iris_Script.py

--- Fisher_Iris_Script.py
+++ Fisher_Iris_Script.py
@@ -1,7 +1,9 @@
 import statistics #ensures Python has all the capabilities to identify and execute statistic functions e.g. statistics.mean()
 import csv #ensures python has all the capabiliites to open and work with csv files 
 
-print ("Petal Length ", "Petal Width ", "Sepal Length ", "Sepal Width")
+print ("Fisher's Iris Data, Sem-1-Python-Project, Eimear Butler April 2018") #print title
+
+print ("Petal Length ", "Petal Width ", "Sepal Length ", "Sepal Width") #print row headers 
 
 with open ("iris.csv") as f:
   for line in f:
@@ -33,9 +35,9 @@ e = "%.3f" % e
 f = statistics.variance(x)
 f = "%.3f" % f
 
-print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,))
-print("The mean of %s is: %s" %(z, a))
-print("The median of %s is: %s" %(z, b))
+print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,)) #print title replacing %s with z (Iris species name)
+print("The mean of %s is: %s" %(z, a)) #print results replacing %s with first z (Iris atribute) then a (value of calculation rounded to 3 decimal places above)
+print("The median of %s is: %s" %(z, b)) #repeat for all functions
 print("The maximum of %s is: %s" %(z, c))
 print("The minimum of %s is: %s" %(z, d))
 print("The standard deviation of %s is: %s" %(z, e))
@@ -43,11 +45,11 @@ print("The standard variance of %s is: %s" %(z, f))
 print()
 
 
-z = "Petal Width" 
-r = 1 
+z = "Petal Width" #z now changes to next Iris atribute 
+r = 1 #new atribute is asociated with next row
 
 
-print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,))
+print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,)) #the same code as row 38-45 can now be used to print results for the new Iris atribute
 print("The mean of %s is: %s" %(z, a))
 print("The median of %s is: %s" %(z, b))
 print("The maximum of %s is: %s" %(z, c))
