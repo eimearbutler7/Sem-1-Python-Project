af47f3ee332fe85e147d99d892e0109e21abac8a
eimearbutler7 <35781826+eimearbutler7@users.noreply.github.com>
2018-04-02 11:55:07 +0100

    Update Fisher_Iris_Script.py

--- Fisher_Iris_Script.py
+++ Fisher_Iris_Script.py
@@ -1,8 +1,11 @@
 import statistics #ensures Python has all the capabilities from the Python Standard Library to identify and execute statistic functions e.g. statistics.mean()
-import csv #ensures python has all the capabiliites from the Python Standard Library to open and work with csv files 
+import csv        #ensures python has all the capabiliites from the Python Standard Library to open and work with csv files
+import matplotlib.pyplot as plt #ensures Python has the capabilities from the Python Standard Library to create a Histogram
+import matplotlib.mlab as mlab #ensures Python has the capabilities from the Python Standard Library to create a Histogram
+
 
 print ("Fisher's Iris Data, Sem-1-Python-Project, Eimear Butler April 2018") #print title
-print()
+print() #space
 print ("Petal Length ", "Petal Width ", "Sepal Length ", "Sepal Width") #print row headers 
 
 with open ("iris.csv") as f:
@@ -25,7 +28,7 @@ x = [float(i) for i in y] #change the 'y' string list, into a floating number ve
 a = statistics.mean(x) # Python Standard Library 9.7. statistics — Mathematical statistics functions
 a = "%.3f" % a     # round to the third decimal point  https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 b = statistics.median(x)
-b = "%.1f" % b
+b = "%.1f" % bsssss
 c = max(x)
 c = "%.1f" % c
 d = min(x)
@@ -35,25 +38,45 @@ e = "%.3f" % e
 f = statistics.variance(x)
 f = "%.3f" % f
 
-print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,)) #print title replacing %s with z (Iris species name)
-print("The mean of %s is: %s" %(z, a)) #print results replacing %s with first z (Iris atribute) then a (value of calculation rounded to 3 decimal places above)
-print("The median of %s is: %s" %(z, b)) #repeat for all functions
+#print results of calculations in rows 28 - 39 in a suitable format 
+print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,))
+print("The mean of %s is: %s" %(z, a))
+print("The median of %s is: %s" %(z, b))
 print("The maximum of %s is: %s" %(z, c))
 print("The minimum of %s is: %s" %(z, d))
 print("The standard deviation of %s is: %s" %(z, e))
 print("The standard variance of %s is: %s" %(z, f))
 print()
+print("A histogram (Figure %s) representing %s distribution has also printed using the matplotlib function" %(r+1, z))
+print()
 
+#create a normalised (normed=1) Hitogram of this atribute using: 
+# matplotlib imported functionality (see also row 3 and 4)
+# # x = same x set created above mon line 24
+mu = statistics.mean(x)
+sigma = statistics.stdev(x)
+bins = 50 #number of bars in Histogram
+n, bins, patches = plt.hist(x, bins, normed=1, facecolor='g', alpha=0.1) #ref: https://plot.ly/matplotlib/histogram facecolour = colour of bars , alpha = increasing in values of 
+
+plt.xlabel('%s' %(z)) #x axis name
+plt.ylabel('Frequency') #y azis name
+plt.title('Histogram of %s'%(z)) #Histogram Title 
+plt.axis([4, 8, 0, 1]) #X axis min and Max, Y axis min and max
+y = mlab.normpdf(bins, mu, sigma) #create histogram data
+plt.plot(bins, y, 'r--') #plot histogram
+
+plt.show() #print plot
 
 z = "Petal Width" #z now changes to next Iris atribute 
-r = 1 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same, only 'z' and 'r' are changed
-y = [] 
+r = 1 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same except the histogram parameter adjustments which were done manually to ensure best representation of data
+y = []  
 with open('iris.csv') as f: 
   for line in f:
     iris = line.split(',')[r] 
-    y.append(iris) 
+    y.append(iris)  
 
 x = [float(i) for i in y] 
+
 
 a = statistics.mean(x) 
 a = "%.3f" % a     
@@ -68,7 +91,7 @@ e = "%.3f" % e
 f = statistics.variance(x)
 f = "%.3f" % f
 
-print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,)) 
+print("From Fisher's Iris Data, the %s has been interrogated as follows:" %(z,))
 print("The mean of %s is: %s" %(z, a))
 print("The median of %s is: %s" %(z, b))
 print("The maximum of %s is: %s" %(z, c))
@@ -76,10 +99,25 @@ print("The minimum of %s is: %s" %(z, d))
 print("The standard deviation of %s is: %s" %(z, e))
 print("The standard variance of %s is: %s" %(z, f))
 print()
+print("A histogram (Figure %s) representing %s distribution has also printed using the matplotlib function" %(r+1, z))
+print()
+ 
+mu = statistics.mean(x)
+sigma = statistics.stdev(x)
+bins = 50 
+n, bins, patches = plt.hist(x, bins, normed=1, facecolor='g', alpha=0.1) 
 
+plt.xlabel('%s' %(z)) 
+plt.ylabel('Frequency') 
+plt.title('Histogram of %s'%(z))  
+plt.axis([2, 5, 0, 2]) #X axis min and Max, Y axis min and max have been MANUALLY adjusted to ensure best representation of data including if normal transformation was needed. I did try to use min (d) and max (c) but this did not work well enough
+y = mlab.normpdf(bins, mu, sigma) 
+plt.plot(bins, y, 'r--') 
+
+plt.show() 
 
 z = "Sepal Length" #z now changes to next Iris atribute 
-r = 2 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same, only 'z' and 'r' are changed
+r = 2 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same except the histogram parameter adjustments which were done manually to ensure best representation of data
 y = [] 
 with open('iris.csv') as f: 
   for line in f:
@@ -87,6 +125,7 @@ with open('iris.csv') as f:
     y.append(iris) 
 
 x = [float(i) for i in y] 
+
 
 a = statistics.mean(x) 
 a = "%.3f" % a     
@@ -109,20 +148,36 @@ print("The minimum of %s is: %s" %(z, d))
 print("The standard deviation of %s is: %s" %(z, e))
 print("The standard variance of %s is: %s" %(z, f))
 print()
+print("A histogram (Figure %s) representing %s distribution has also printed using the matplotlib function" %(r+1, z))
+print()
 
+mu = statistics.mean(x)
+sigma = statistics.stdev(x)
+bins = 50 
+n, bins, patches = plt.hist(x, bins, normed=1, facecolor='g', alpha=0.1) 
 
-z = "Sepal Width" #z now changes to next Iris atribute
-r = 3 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same, only 'z' and 'r' are changed
-y = [] 
+plt.xlabel('%s' %(z)) 
+plt.ylabel('Frequency') 
+plt.title('Histogram of %s'%(z))  
+plt.axis([0, 7, 0, 1]) #X axis min and Max, Y axis min and max have been MANUALLY adjusted to ensure best representation of data including if normal transformation was needed. I did try to use min (d) and max (c) but this did not work well enough
+y = mlab.normpdf(bins, mu, sigma) 
+plt.plot(bins, y, 'r--') 
+
+plt.show() 
+
+z = "Sepal Width" #z now changes to next Iris atribute 
+r = 3 #new atribute is asociated with next row #all the rest of this script for Petal Width remains the same except the histogram parameter adjustments which were done manually to ensure best representation of data
+y = []  
 with open('iris.csv') as f: 
   for line in f:
     iris = line.split(',')[r] 
-    y.append(iris) 
+    y.append(iris)  
 
 x = [float(i) for i in y] 
 
+
 a = statistics.mean(x) 
-a = "%.3f" % a     
+a = "%.3f" % a    
 b = statistics.median(x)
 b = "%.1f" % b
 c = max(x)
@@ -141,3 +196,20 @@ print("The maximum of %s is: %s" %(z, c))
 print("The minimum of %s is: %s" %(z, d))
 print("The standard deviation of %s is: %s" %(z, e))
 print("The standard variance of %s is: %s" %(z, f))
+print()
+print("A histogram (Figure %s) representing %s distribution has also printed using the matplotlib function" %(r+1, z))
+print()
+
+mu = statistics.mean(x)
+sigma = statistics.stdev(x)
+bins = 50 
+n, bins, patches = plt.hist(x, bins, normed=1, facecolor='g', alpha=0.1) 
+
+plt.xlabel('%s' %(z)) 
+plt.ylabel('Frequency') 
+plt.title('Histogram of %s'%(z)) 
+plt.axis([0, 3, 0, 4]) #X axis min and Max, Y axis min and max have been MANUALLY adjusted to ensure best representation of data including if normal transformation was needed. I did try to use min (d) and max (c) but this did not work well enough
+y = mlab.normpdf(bins, mu, sigma)
+plt.plot(bins, y, 'r--') 
+
+plt.show() 
